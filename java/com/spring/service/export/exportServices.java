86426d3e5ec9ae6a0258c5acb26f0f5026db62ec
binhhtph11879 <67915869+binhhtph1179@users.noreply.github.com>
2021-12-08 16:58:19 +0700

    ff

--- java/com/spring/service/export/exportServices.java
+++ java/com/spring/service/export/exportServices.java
@@ -243,25 +243,25 @@ public class exportServices {
 		return result.toString();
 	}
 
-	private static CTAnchor getAnchorWithGraphic(CTGraphicalObject graphicalobject, String drawingDescr, int width,
-			int height, int left, int top) throws Exception {
-
-		String anchorXML = "<wp:anchor xmlns:wp=\"http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing\" "
-				+ "simplePos=\"0\" relativeHeight=\"0\" behindDoc=\"1\" locked=\"0\" layoutInCell=\"1\" allowOverlap=\"1\">"
-				+ "<wp:simplePos x=\"0\" y=\"0\"/>" + "<wp:positionH relativeFrom=\"column\"><wp:posOffset>" + left
-				+ "</wp:posOffset></wp:positionH>" + "<wp:positionV relativeFrom=\"paragraph\"><wp:posOffset>" + top
-				+ "</wp:posOffset></wp:positionV>" + "<wp:extent cx=\"" + width + "\" cy=\"" + height + "\"/>"
-				+ "<wp:effectExtent l=\"0\" t=\"0\" r=\"0\" b=\"0\"/>" + "<wp:wrapTight wrapText=\"bothSides\">"
-				+ "<wp:wrapPolygon edited=\"0\">" + "<wp:start x=\"0\" y=\"0\"/>" + "<wp:lineTo x=\"0\" y=\"21600\"/>"
-				// Square// polygon// 21600// x// 21600// leads// to// wrap// points// in// fully// width// x// height
-				+ "<wp:lineTo x=\"21600\" y=\"21600\"/>"// Why? I don't know. Try & error ;-).
-				+ "<wp:lineTo x=\"21600\" y=\"0\"/>" + "<wp:lineTo x=\"0\" y=\"0\"/>" + "</wp:wrapPolygon>"
-				+ "</wp:wrapTight>" + "<wp:docPr id=\"1\" name=\"Drawing 0\" descr=\"" + drawingDescr
-				+ "\"/><wp:cNvGraphicFramePr/>" + "</wp:anchor>";
-
-		CTDrawing drawing = CTDrawing.Factory.parse(anchorXML);
-		CTAnchor anchor = drawing.getAnchorArray(0);
-		anchor.setGraphic(graphicalobject);
-		return anchor;
-	}
+//	private static CTAnchor getAnchorWithGraphic(CTGraphicalObject graphicalobject, String drawingDescr, int width,
+//			int height, int left, int top) throws Exception {
+//
+//		String anchorXML = "<wp:anchor xmlns:wp=\"http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing\" "
+//				+ "simplePos=\"0\" relativeHeight=\"0\" behindDoc=\"1\" locked=\"0\" layoutInCell=\"1\" allowOverlap=\"1\">"
+//				+ "<wp:simplePos x=\"0\" y=\"0\"/>" + "<wp:positionH relativeFrom=\"column\"><wp:posOffset>" + left
+//				+ "</wp:posOffset></wp:positionH>" + "<wp:positionV relativeFrom=\"paragraph\"><wp:posOffset>" + top
+//				+ "</wp:posOffset></wp:positionV>" + "<wp:extent cx=\"" + width + "\" cy=\"" + height + "\"/>"
+//				+ "<wp:effectExtent l=\"0\" t=\"0\" r=\"0\" b=\"0\"/>" + "<wp:wrapTight wrapText=\"bothSides\">"
+//				+ "<wp:wrapPolygon edited=\"0\">" + "<wp:start x=\"0\" y=\"0\"/>" + "<wp:lineTo x=\"0\" y=\"21600\"/>"
+//				// Square// polygon// 21600// x// 21600// leads// to// wrap// points// in// fully// width// x// height
+//				+ "<wp:lineTo x=\"21600\" y=\"21600\"/>"// Why? I don't know. Try & error ;-).
+//				+ "<wp:lineTo x=\"21600\" y=\"0\"/>" + "<wp:lineTo x=\"0\" y=\"0\"/>" + "</wp:wrapPolygon>"
+//				+ "</wp:wrapTight>" + "<wp:docPr id=\"1\" name=\"Drawing 0\" descr=\"" + drawingDescr
+//				+ "\"/><wp:cNvGraphicFramePr/>" + "</wp:anchor>";
+//
+//		CTDrawing drawing = CTDrawing.Factory.parse(anchorXML);
+//		CTAnchor anchor = drawing.getAnchorArray(0);
+//		anchor.setGraphic(graphicalobject);
+//		return anchor;
+//	}
 }
