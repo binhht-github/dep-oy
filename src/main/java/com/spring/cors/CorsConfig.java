97167142b7066d20a69b0772b837498f073c42b1
binhhtph11879 <67915869+binhhtph1179@users.noreply.github.com>
2021-12-13 08:57:59 +0700

    a

--- src/main/java/com/spring/cors/CorsConfig.java
+++ src/main/java/com/spring/cors/CorsConfig.java
@@ -1,16 +1,16 @@
 //package com.spring.cors;
-
+//
 //import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//
 //@Configuration
 //public class CorsConfig  implements WebMvcConfigurer {
-
+//
 //        @Value("${allowed.origin}")
-//      private String allowedOrigin;
-
+//        private String allowedOrigin;
+//
 //        @Override
 //        public void addCorsMappings(CorsRegistry registry) {
 //            registry.addMapping("/**")
@@ -18,5 +18,5 @@
 //                    .allowedMethods("*")
 //                    .allowedHeaders("*")
 //                    .allowCredentials(true);
-        }
+//        }
 //}
